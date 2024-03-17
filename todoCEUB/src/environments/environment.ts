59c5fd41f98fc0a1d6715db21c566c518cb4d09f
Lucas Zoser <53053622+Zosin0@users.noreply.github.com>
2024-03-17 14:22:28 -0300

    Update environment.ts

--- todoCEUB/src/environments/environment.ts
+++ todoCEUB/src/environments/environment.ts
@@ -1,11 +1,6 @@
 export const environment = {
     production: false,
     firebaseConfig: {
-        apiKey: "AIzaSyCDepP5Si6S5ukiNp-IcyfN2hOlIxtZ5yM",
-        authDomain: "ceub-todo.firebaseapp.com",
-        projectId: "ceub-todo",
-        storageBucket: "ceub-todo.appspot.com",
-        messagingSenderId: "35568808254",
-        appId: "1:35568808254:web:40c7c0753ff1db48143c6f"
+
     }
 };
